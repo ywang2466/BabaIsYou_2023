a68983b37b98ea82e3208536a0d389040b0ddb15
ashiqwisc <ashiq@wisc.edu>
2023-03-08 20:35:53 -0600

    Update from_raw_to_readable.R

--- code/from_raw_to_readable.R
+++ code/from_raw_to_readable.R
@@ -14,18 +14,45 @@ df
 # # Filter out invalid timestamps
 # df <- df[df$Start >= 0,]
 
-# Re-coordinate the timestamp so we don't have negative timestamps
-### Muhammad
-
-# Use level one as example
-# df <- df[df$Level == "01 - Where Do I Go?", ]
-
-# Sort by playerID, levelID, stuckID, start time
+# Pull numerical values out of strings
 df$PlayerID <- as.numeric(sub('Player ','', df$PlayerID)) 
 df$StuckID <- as.numeric(sub('Stuck','', df$StuckID)) 
 df$LevelID <- ifelse(df$Level == "02 - Now What Is This?", 2, 1)
 
+# Re-coordinate the timestamp so we don't have negative timestamps
+### Muhammad 
 
+# Attach a group_ID to each group, a negative boolean to each case (T if neg, F if pos)
+df <- df %>%
+  ungroup() %>%
+  group_by(PlayerID, StuckID, Learned, LevelID) %>%
+  mutate(group_ID = cur_group_id(), neg = ifelse(Start < 0 | End < 0, TRUE, FALSE)) 
+
+# Get addends
+addends <- df %>%
+  filter(neg == TRUE) %>%
+  group_by(group_ID) %>%
+  summarize(addend = abs(Start), group_ID) %>%
+  distinct()
+ 
+# Join addends to df so that all of df is preserved 
+df <- left_join(df, addends, by = "group_ID")
+  
+# Set NA addends to 0, and add addend for each group to each case
+df <- df %>%
+  mutate(addend = ifelse(is.na(addend), 0, addend)) %>%
+  mutate(Start = Start + addend, End = End + addend) %>%
+  select(-c(neg, addend)) # groupID removed, uncomment for demo purposes 
+
+
+### Each session  has a different current (negative) origin value. Figure out a way to set origin to 0 and recalibrate the 
+### rest of the timeline 
+
+# Use level one as example
+# df <- df[df$Level == "01 - Where Do I Go?", ]
+
+
+# Sort by playerID, levelID, stuckID, start time
 df <- df[order(df$PlayerID,df$LevelID, df$StuckID, df$Learned, df$Start),]
 # View(df)
 colnames(df)
